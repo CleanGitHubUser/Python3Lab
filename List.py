54c6502f995638f6d11b890dfdba4240100ea8a1
FatalWhite <34561520+fatalwhite@users.noreply.github.com>
2018-01-24 15:10:37 +0900

    2018-01-24

--- List.py
+++ List.py
@@ -32,4 +32,75 @@ print( list2_list + list3_list )
 print( list2_list * 2 )
 
 # 요소의 특정값 참조 : index 사용
+print( msg[4], msg[9] )
+print( list2_list[2] )
+print( list3_list[2] )
+print( list4_list[3] )
 
+# 요소값 변경 : index, = 사용
+list2_list[2] = -3
+print( list2_list )
+
+# 주민코드에서 성별 여부 판별
+jumin = [ 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 7]
+if jumin[6] % 2 == 1:
+    print( '남자입니다.' )
+elif jumin[6] % 2 == 0:
+    print( '여자입니다.' )
+
+# 주민코드에서 생년월일 추출
+for i in range( 0 , 6 ):
+    print( jumin[i], end = ' ' )
+        # 줄바꿈 없이 출력시 종결문자를 지정
+print()
+
+# 특정범위내 요소들을 추출할때는 슬라이스를 사용 [ i : j : step ]
+print( jumin[ 0 : 6 ] )         # 생년월일
+print( jumin[ : 6 ] )
+print( jumin[ 6 : ] )           # 생년월일 제외 나머지부분
+print( jumin[ : ] )             # 모두
+
+print( jumin[ 0 : 6 : 2 ] )     # 홀수자리만 추출
+print( jumin[ :: -1 ] )         # 역순 출력
+
+print( jumin[ 0 : 100 : 2 ] )   # 인덱스 초과 - 오류?
+# print( jumin[ 100 ] )           # 인덱스 초과 - 오류?
+
+# 리스트 관련 통계함수
+print( sum( list2_list ) )
+print( min( list2_list ) )
+print( max( list2_list ) )
+
+# 리스트가 주어지면
+# 이것의 가운데에 있는 요소값을 출력
+# [ 1, 2, 6, 8, 4 ] => 6
+# [ 1, 3, 6, 8, 4, 10 ] => 6, 8
+slist1 = [ 1, 2, 6, 8, 4 ]
+slist2 = [ 1, 3, 6, 8, 4, 10 ]
+
+# list = [ 1, 2, 3, 4, 5 ]
+list = [ 1, 2, 3, 4, 5, 6 ]
+size = len( list )
+mid = int( size / 2 )
+# print( '가운데 값 : ', list[ mid ] )  # 요소 수가 홀수
+print( '가운데 값 : ', list[ mid - 1 : mid + 1 ] )
+
+def centList(list):
+    if len( list ) % 2 == 0:
+        cent = int ( len( list ) / 2 - 1 )
+        return list[ cent : cent + 2 ]
+    elif len( list ) % 2 == 1:
+        cent = int( ( len( list ) - 1 ) / 2 )
+        return list[ cent ]
+
+print( centList( slist1 ) )
+print( centList( slist2 ) )
+
+# 요소 추가 : append
+list = [ 1,2, 3, 4, 5 ]
+list.append(9)
+list.append(8)
+print( list )
+
+# 요소 추가 : insert(위치, 값)
+list.index()
